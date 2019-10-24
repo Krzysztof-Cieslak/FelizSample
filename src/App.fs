095c191f08fad4a422478cde1a1f5b77a7b2dc39
Samuel Berger <samuel.berger@icfm.ch>
2019-10-24 17:38:56 +0200

    MaterialUI sample

--- src/App.fs
+++ src/App.fs
@@ -3,6 +3,7 @@ module App
 open Elmish
 open Elmish.React
 open Feliz
+open Feliz.MaterialUI
 
 type State = { Count: int }
 
@@ -17,7 +18,8 @@ let update (msg: Msg) (state: State): State =
     | Increment -> { state with Count = state.Count + 1 }
     | Decrement -> { state with Count = state.Count - 1 }
 
-let render (state: State) (dispatch: Msg -> unit) =
+let render (state: State) (dispatch: Msg -> unit) = 
+
   Html.div [
     Html.button [
       prop.onClick (fun _ -> dispatch Increment)
@@ -30,6 +32,18 @@ let render (state: State) (dispatch: Msg -> unit) =
     ]
 
     Html.h1 state.Count
+    Mui.button [ button.fullWidth true; prop.text "BCDx"; button.variant.contained; button.color.primary]
+    Mui.grid [ 
+      grid.container true;
+      grid.spacing._2 
+      grid.children [
+        Mui.grid [ 
+          grid.item true 
+          grid.xs._12
+          grid.children [ Mui.button [ button.fullWidth true; prop.text "BCD"; button.variant.contained; button.color.primary]]
+        ]
+      ]
+    ]
   ]
 
 Program.mkSimple init update render
--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ var CONFIG = {
     cssEntry: "./sass/main.sass",
     outputDir: "./deploy",
     assetsDir: "./public",
-    devServerPort: 8080,
+    devServerPort: 8081,
     // When using webpack-dev-server, you may need to redirect some calls
     // to a external API server. See https://webpack.js.org/configuration/dev-server/#devserver-proxy
     devServerProxy: undefined,
